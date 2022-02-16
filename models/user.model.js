2920264fe6b6ff270645c68f1c7cba164035dc45
Andres Arevalo <andresfelipear@gmail.com>
2022-02-16 14:20:07 -0800

    modified model user to add jwt

--- models/user.model.js
+++ models/user.model.js
@@ -1,11 +1,34 @@
+const passportLocalMongoose = require('passport-local-mongoose')
 const {Schema, model} = require("../db/connection") 
 
 const UserSchema = new Schema({
     username: {type: String, unique: true, required: true},
     email: {type: String, unique: true, required: true},
-    password: {type: String, required: true}
+    password: {type: String, required: true},
+    authStrategy: {
+        type:String,
+        default: 'local'
+    },
+    refreshToken: {
+        type: [Session]
+    },
+    points: {
+        type: Number,
+        defautl:50
+    }
 })
 
+
+
+//remove refreshtoken from the response
+UserSchema.set("toJSON", {
+    transform: function(doc, ret, options){
+        delete ret.refreshToken
+        return ret
+    }
+})
+
+UserSchema.plugin(passportLocalMongoose)
 
 const User = model("User", UserSchema)
 
